854e00eb206fc6750f68ff3a8f118f18f81301cf
EgorAnti <egor.anti.00@mail.ru>
2025-07-16 11:11:39 +0300

    add all walls in second floor

--- core/src/main/java/com/elevator_project/second_floor/SecondFloor.java
+++ core/src/main/java/com/elevator_project/second_floor/SecondFloor.java
@@ -1,9 +1,14 @@
 package com.elevator_project.second_floor;
 
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.elevator_project.extra_elements.Arrows;
+import com.elevator_project.extra_elements.DownArrow;
+import com.elevator_project.extra_elements.Inventory;
 import com.elevator_project.game.App;
 import com.elevator_project.game.Floor;
+import com.elevator_project.game.GameManager;
 import com.elevator_project.game.RoomPart;
+import com.elevator_project.independent_elements.Door;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,16 +18,35 @@ public class SecondFloor implements Floor {
     private final List<RoomPart> parts;
     private final List<Group> groups;
     private SecondFloorFirstSide firstSide;
+    private SecondFloorSecondSide secondSide;
+    private SecondFloorThirdSide thirdSide;
+    private SecondFloorFourthSide fourthSide;
+    private final Door door;
+    private final Inventory inventory;
+    private final Arrows arrows;
+    private final DownArrow downArrow;
+    private int partIndex;
 
     public SecondFloor() {
         parts = new ArrayList<>();
         groups = new ArrayList<>();
+        door = GameManager.getDoor();
+        inventory = GameManager.getInventory();
+        arrows = GameManager.getArrows();
+        downArrow = GameManager.getDownArrow();
+        partIndex = 0;
         initParts();
     }
 
     private void initParts() {
         firstSide = new SecondFloorFirstSide();
         parts.add(firstSide);
+        secondSide = new SecondFloorSecondSide();
+        parts.add(secondSide);
+        thirdSide = new SecondFloorThirdSide();
+        parts.add(thirdSide);
+        fourthSide = new SecondFloorFourthSide();
+        parts.add(fourthSide);
     }
 
     private void initGroups() {
@@ -33,12 +57,12 @@ public class SecondFloor implements Floor {
 
     @Override
     public void right() {
-
+        move(partIndex, (partIndex + 1) % 4);
     }
 
     @Override
     public void left() {
-
+        move(partIndex, (partIndex + 3) % 4);
     }
 
     @Override
@@ -51,12 +75,31 @@ public class SecondFloor implements Floor {
 
     }
 
+    private void move (int hide, int show) {
+        App.getSoundManager().playSteps();
+        if (hide == 2) {
+            door.hide();
+        }
+        partIndex = show;
+        groups.get(hide).setVisible(false);
+        groups.get(show).setVisible(true);
+        if (show == 2) {
+            door.show();
+        }
+    }
+
     @Override
     public void render() {
         initGroups();
         for (Group group : groups) {
             App.getStage().addActor(group);
         }
+        inventory.render();
+        door.render();
+        arrows.render();
+        downArrow.render();
+        downArrow.hide();
+        move(2, 0);
     }
 
     @Override
@@ -64,6 +107,10 @@ public class SecondFloor implements Floor {
         for (Group group : groups) {
             group.remove();
         }
+        inventory.dispose();
+        door.dispose();
+        arrows.dispose();
+        downArrow.dispose();
     }
 
     @Override
@@ -71,12 +118,16 @@ public class SecondFloor implements Floor {
         for (Group group : groups) {
             group.setVisible(false);
         }
+        door.hide();
     }
 
     @Override
     public void show() {
         for (Group group : groups) {
             group.setVisible(true);
+        }
+        if (partIndex == 2) {
+            door.show();
         }
     }
 }
---
+++ core/src/main/java/com/elevator_project/second_floor/SecondFloorThirdSide.java
@@ -0,0 +1,43 @@
+package com.elevator_project.second_floor;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.elevator_project.game.GameManager;
+import com.elevator_project.game.ImageProcessing;
+import com.elevator_project.game.RoomPart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondFloorThirdSide extends RoomPart {
+
+    private final TextureAtlas atlas;
+    private final List<Image> elements;
+
+    public SecondFloorThirdSide () {
+        atlas = GameManager.getAtlasses().getSecondFloorAtlas();
+        elements = new ArrayList<>();
+        initElements();
+    }
+
+    private void initElements() {
+        elements.add(initWall());
+    }
+
+    private Image initWall () {
+        float WALL_RESIZE_FACTOR = 158f;
+
+        Image wall = new Image(atlas.createSprite("Wall", 1));
+        ImageProcessing.process(wall, WALL_RESIZE_FACTOR, w, h);
+
+        return wall;
+    }
+
+    public Group initGroup() {
+        for (Image element : elements) {
+            mainGroup.addActor(element);
+        }
+        return mainGroup;
+    }
+}
